ad0104ead4453f8d7cfdca19ccd82eef45039e3e
badrabdoph-netizen <badrabdoph@icloud.com>
2026-07-14 05:03:54 +0300

    b

--- src/modules/guidance/index.ts
+++
@@ -1,3 +0,0 @@
-export { GuidanceProvider, useGuidance } from "./guidance-context";
-export { analyzeSiteState, isSiteComplete, calculateProgress } from "./smart-decision-engine";
-export type { SiteState, Recommendation } from "./smart-decision-engine";
